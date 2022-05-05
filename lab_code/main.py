7375619bd194f91acee531a8f9f1ce993540a160
HP <viktor_b1@rambler.ru>
2022-05-05 17:10:34 +0300

    add structure

--- lab_code/main.py
+++ lab_code/main.py
@@ -4,35 +4,8 @@ import itertools
 from BFC import findpaths
 import properties
 from combination import comb
+from structure import ring_structure
 from timeCombination import DFS
-
-ring_structure = [[] for _ in range(properties.n_edge)]
-# Construct a graph
-ring_structure[0].append(1)
-ring_structure[0].append(9)
-ring_structure[1].append(0)
-ring_structure[1].append(2)
-ring_structure[2].append(1)
-ring_structure[2].append(3)
-ring_structure[3].append(2)
-ring_structure[3].append(4)
-ring_structure[4].append(3)
-ring_structure[4].append(5)
-ring_structure[5].append(4)
-ring_structure[5].append(6)
-ring_structure[6].append(5)
-ring_structure[6].append(7)
-ring_structure[7].append(6)
-ring_structure[7].append(8)
-ring_structure[8].append(7)
-ring_structure[8].append(9)
-ring_structure[9].append(8)
-ring_structure[9].append(1)
-# добавлениеновых путей
-#ring_structure[0].append(3)
-#ring_structure[2].append(8)
-#ring_structure[5].append(9)
-#ring_structure[7].append(4)
 
 src = 2
 dst = 3
