b2b9b6042fa86beb098229bc52aecc1a0357fa1d
HP <viktor_b1@rambler.ru>
2022-05-12 16:23:55 +0300

    add bindings

--- lab_code/structure.py
+++ lab_code/structure.py
@@ -1,6 +1,13 @@
 import properties
 
+Lij = [1000] * 10
+L = {}
+bindings = {}
 ring_structure = [[] for _ in range(properties.n_edge)]
+ring = [[] for _ in range(properties.n_edge+properties.n_vertex)]
+# список связей
+connections = [i+properties.n_vertex for i in range(properties.n_vertex)]
+
 # Construct a graph
 ring_structure[0].append(1)
 ring_structure[0].append(9)
@@ -27,3 +34,16 @@ ring_structure[9].append(1)
 #ring_structure[2].append(8)
 #ring_structure[5].append(9)
 #ring_structure[7].append(4)
+
+# добавим пути в структуру
+
+
+# длинна связей вершин графа
+for i, el in enumerate(Lij):
+    L[str(ring_structure[i])] = el
+    bindings[connections[i]] = ring_structure[i]
+
+if __name__ == "__main__":
+	print(L)
+	print("\n")
+	print(bindings)
